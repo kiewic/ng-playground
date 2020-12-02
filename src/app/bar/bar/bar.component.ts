513bee64138dad2a26d6b5b32b3d9c27feb07be0
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2020-12-02 02:30:50 -0800

    Link BarComponent and RandomService

--- src/app/bar/bar/bar.component.ts
+++ src/app/bar/bar/bar.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { RandomServiceToken, IRandomService } from '../../random-service-token';
 
 @Component({
   selector: 'app-bar',
   template: `
     <p>
-      bar works!
+      bar is {{randomValue}}!
     </p>
   `,
   styles: [
   ]
 })
 export class BarComponent implements OnInit {
+  readonly randomValue: number;
 
-  constructor() { }
+  constructor(@Inject(RandomServiceToken) random: IRandomService) {
+    this.randomValue = random.getRandomValue();
+  }
 
   ngOnInit(): void {
   }
